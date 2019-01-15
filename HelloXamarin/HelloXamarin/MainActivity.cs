b9506c1dc139522e8d28700918a4d3ca667c1707
prudx <dnl.maguire@gmail.com>
2019-01-15 14:14:47 +0000

    new private repository

---
+++ HelloXamarin/HelloXamarin/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace HelloXamarin
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+        int txtNumberCount;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+                txtNumber.Text = (++txtNumberCount).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+                txtNumber.Text = (--txtNumberCount).ToString();
+        }
+    }
+}
